550c7bf13c434af2dd2b0d9f8c4dd7bd1a3b5e57
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-14 15:14:32 +0200

    added functionality to both controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,53 @@
 class ItemsController < ApplicationController
+  before_action :set_category
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
+    @items = Category.items
   end
 
   def show
   end
 
   def new
+    @item = Category.items.build
   end
 
   def create
+    @item = @category.item.build(item_params)
+    if @item.save
+      redirect_to category_items_path(@category), notice: "Item created"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: "Item updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @item.destroy
+    redirect_to category_items_path(@item.category), notice: "Item destroyed"
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :quantity, :unit, :color)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/create'
-  get 'items/edit'
-  get 'items/update'
-  get 'items/destroy'
-
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
-
   root 'categories#index'
-  resources :categories do
+  resources :categories, only: [:index, :show] do
     resources :items
   end
 end
