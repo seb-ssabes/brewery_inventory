4694dae16dcd872103a249d77c1e90c10d1f69f5
Sebastian Olivares <olivares.heredia@gmail.com>
2024-09-24 13:35:36 +0200

    hop show/edit tweaked

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
+    './app/**/*.html.erb',
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/assets/stylesheets/**/*.css',
   ],
   theme: {
     extend: {
