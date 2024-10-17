e257719c04c5803f307535c89eeb3638e9f3032b
Sebastian Olivares <olivares.heredia@gmail.com>
2024-10-17 13:11:41 +0200

    testing card controller with click touchstart

--- app/javascript/controllers/card_controller.js
+++ app/javascript/controllers/card_controller.js
@@ -3,63 +3,59 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["card"]
 
-  isClicked = false;
+  isTouching = false;
 
   connect() {
     console.log("Card controller connected");
 
     this.cardTargets.forEach(card => {
-      card.addEventListener('click', this.handleClick.bind(this));
-      // card.addEventListener('touchend', this.touchend.bind(this));
+      card.addEventListener('touchstart', this.touchstart.bind(this));
+      card.addEventListener('touchend', this.touchend.bind(this));
     });
   }
 
-  handleClick(event) {
-    console.log("Card clicked");
+  touchstart(event) {
+    console.log("touchstart");
 
-    if (this.isClicked) {
+    if (this.isTouching) {
       console.log("Already touching, skipping.");
       return;
     }
-    this.isClicked = true;
+    this.isTouching = true;
 
-    // event.preventDefault();
+    event.preventDefault();
 
     event.currentTarget.classList.add("item-card-active");
+  }
+
+  touchend(event) {
+    console.log("touchend");
+    event.stopPropagation();
+
+    if (!this.isTouching) {
+      console.log("No valid touch to end");
+      return;
+    }
+    this.isTouching = false
+
+    this.clearTouchFeedback(event);
+
+    const card = event.currentTarget;
 
     setTimeout(() => {
       window.location.href = card.dataset.url;
     }, 100);
+
+    if (this.navigating) {
+      console.log("Already navigating, skipping");
+      return;
+    }
+    this.navigating = true;
+    window.location.href = card.dataset.url;
   }
 
-  // touchend(event) {
-  //   console.log("touchend");
-  //   event.stopPropagation();
-
-  //   if (!this.isTouching) {
-  //     console.log("No valid touch to end");
-  //     return;
-  //   }
-  //   this.isTouching = false
-
-  //   this.clearTouchFeedback(event);
-
-  //   const card = event.currentTarget;
-
-  //   setTimeout(() => {
-  //     window.location.href = card.dataset.url;
-  //   }, 100);
-
-  //   if (this.navigating) {
-  //     console.log("Already navigating, skipping");
-  //     return;
-  //   }
-  //   this.navigating = true;
-  //   window.location.href = card.dataset.url;
-  // }
-
-  // clearTouchFeedback(event) {
-  //   event.currentTarget.classList.remove("item-card-active");
-  //   console.log("Removed active class and cleared timeout");
-  // }
+  clearTouchFeedback(event) {
+    event.currentTarget.classList.remove("item-card-active");
+    console.log("Removed active class and cleared timeout");
+  }
 }
