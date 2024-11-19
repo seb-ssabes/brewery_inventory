0e40a7a5dff5817cbbe4a2394fe84b5215b2d5e6
Sebastian Olivares <olivares.heredia@gmail.com>
2024-11-19 12:12:34 +0100

    abv calculator done

--- app/javascript/controllers/abv_controller.js
+++ app/javascript/controllers/abv_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["og", "fg", "abv_result"]
+  static targets = ["og", "fg", "abv_result", "abv_title", "abvCalculate"]
 
   connect() {
     console.log("ABV controller connected")
@@ -16,6 +16,13 @@ export default class extends Controller {
 
     const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
 
+    this.abvCalculateTarget.classList.add("ring-8", "ring-gray-300",
+                                          "transition-all", "duration-500", "ease-in-out");
+
+    setTimeout(() => {
+      this.abvCalculateTarget.classList.remove("ring-8", "ring-gray-300");
+    }, 500);
+
     fetch("/calculators/calculate_abv", {
       method: "POST",
       headers: {
@@ -27,7 +34,19 @@ export default class extends Controller {
     })
       .then((response) => response.json())
       .then((data) => {
-        this.abv_resultTarget.textContent = `ABV: ${data.abv}%`;
+        this.abv_titleTarget.textContent = `ABV:`;
+        this.abv_resultTarget.textContent = `${data.abv}%`;
+
+        this.abv_titleTarget.classList.remove("hidden");
+        this.abv_resultTarget.classList.remove("hidden");
+
+        this.abv_titleTarget.classList.add("slide-up");
+        this.abv_resultTarget.classList.add("slide-up");
+
+        setTimeout(() => {
+          this.abv_titleTarget.classList.remove("slide-up");
+          this.abv_resultTarget.classList.remove("slide-up");
+        }, 1000);
       })
       .catch((error) => {
         console.error("ABV calculation error:", error);
