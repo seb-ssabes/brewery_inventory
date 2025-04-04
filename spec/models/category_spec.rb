bbd3f6916f5404a8dd43ebaac5a0e4c8ea52fa19
Sebastian Olivares <olivares.heredia@gmail.com>
2025-04-04 12:10:50 +0200

    category model rspec done

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Category do
-  it "has a name" do
-    category = Category.create!(name: "Malts")
-    expect(category.name).to eq("Malts")
+  describe "associations" do
+    it "has many items" do
+      category = Category.create!(name: "Barley")
+      item1 = category.items.create!(name: "Wheat1", quantity: 30, price: 2000, origin: "Germany", alpha: 7.5)
+      item2 = category.items.create!(name: "Wheat2", quantity: 40, price: 3000, origin: "Australia", alpha: 9.5)
+      expect(category.items).to include(item1, item2)
+    end
+
+    it "destroys associated items when deleted" do
+      category = Category.create!(name: "Malts")
+      item = category.items.create!(name: "Malt1", quantity: 50, price: 5000, origin: "Australia", alpha: 8.5)
+      expect { category.destroy }.to change {Item.count}.by(-1)
+    end
   end
 
   describe "validations" do
+    it "is valid with a name" do
+      category = Category.new(name: "Yeasts")
+      expect(category).to be_valid
+    end
+
     it "is invalid without a name" do
       category = Category.new(name: nil)
-      expect(category).to_not be_valid
-      expect(category.errors[:name]).to include("can't be blank")
+
+      expect(category).not_to be_valid
+      expect(category.errors[:name]).not_to be_empty
     end
 
     it "is invalid with a duplicate name" do
       Category.create!(name: "Malts")
       duplicate_category = Category.new(name: "Malts")
-      # duplicate_category = Category.new(name: "Hops")
 
-      expect(duplicate_category).to_not be_valid
-      expect(duplicate_category.errors[:name]).to include("has already been taken")
+      expect(duplicate_category).not_to be_valid
+      expect(duplicate_category.errors[:name]).not_to be_empty
     end
   end
 end
